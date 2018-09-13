459c530c95e3ada5c857fc572856167fd111f006
lnki96 <lnki96@hotmail.com>
2018-09-14 02:08:44 +0800

    Add an Activity.

---
+++ app/src/main/java/me/lnki96/op/wifidemo/MainActivity.java
@@ -0,0 +1,13 @@
+package me.lnki96.op.wifidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
